2afea234b61dbb48e99f1ecdb61a4d26c1d87de5
fmaylinch <ferranmaylinch@gmail.com>
2018-12-17 12:27:45 +0100

    Add Handlebars template

--- src/main/java/tech/bts/counters/CounterController.java
+++ src/main/java/tech/bts/counters/CounterController.java
@@ -1,11 +1,15 @@
 package tech.bts.counters;
 
+import com.github.jknack.handlebars.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class CounterController {
@@ -18,10 +22,15 @@ public class CounterController {
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/")
-    public String viewCounters() {
+    public String viewCounters() throws IOException {
 
         Collection<Counter> counters = counterService.getAllCounters();
 
-        return "Number of counters: <strong>" + counters.size() + "</strong>";
+        Map<String, Object> values = new HashMap<>();
+        values.put("value", counters.size());
+
+        Template template = HandlebarsUtil.compile("counters");
+
+        return template.apply(values);
     }
 }
